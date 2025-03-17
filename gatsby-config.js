48a71e1649fbd263a23b9ae00d67287edcbc7ffa
Erçetin Güler <ercetinguler@hotmail.com>
2025-03-17 12:30:28 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,3 +9,19 @@ module.exports = {
         },
     ]
 }
+import { Disqus } from 'gatsby-plugin-disqus';
+
+const Template = () => (
+    /* Page contents */
+
+    <Disqus
+        config={
+            /* Replace PAGE_URL with your post's canonical URL variable */
+            url: 'PAGE_URL',
+            /* Replace PAGE_IDENTIFIER with your page's unique identifier variable */
+            identifier: 'PAGE_IDENTIFIER',
+            /* Replace PAGE_TITLE with the title of the page */
+            title: 'PAGE_TITLE',
+        }
+    />
+);
